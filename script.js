d3964abb44f6e1b10b27be3e1a33dbf9ee71e9b2
redo_oreon <sokolovskii.vlad2011@mail.ru>
2022-09-14 18:36:28 +0300

    update user interface functionality

--- script.js
+++ script.js
@@ -8,14 +8,27 @@ const personalMovieDB = {
   private: false,
 };
 
-let lastWatchedMovie = prompt('What movie you have watched last?');
-let movieRaiting = prompt('How can you rate the film?');
-personalMovieDB.movies[lastWatchedMovie] = movieRaiting;
+for (let i = 0; i < 2; i++) {
+  const a = prompt('Один из последних просмотренных фильмов?', ''),
+    b = prompt('На сколько оцените его?', '');
 
-lastWatchedMovie = prompt('What movie you have watched last?');
-movieRaiting = prompt('How can you rate the film?');
-personalMovieDB.movies[lastWatchedMovie] = movieRaiting;
+  if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+    personalMovieDB.movies[a] = b;
+    console.log('done');
+  } else {
+    console.log('error');
+    i--;
+  }
+}
 
-personalMovieDB.movies[lastWatchedMovie] = movieRaiting;
+if (personalMovieDB.count < 10) {
+  console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+  console.log('Вы классический зритель');
+} else if (personalMovieDB.count >= 30) {
+  console.log('Вы киноман');
+} else {
+  console.log('Произошла ошибка');
+}
+
 console.log(personalMovieDB);
-
